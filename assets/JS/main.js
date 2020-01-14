7139701522a92cc0cbd58f7f42d2b97cbe76ce12
Elouan <elouanmace.professionnel@gmail.com>
2020-01-14 09:41:42 +0100

    40%

---
+++ assets/JS/main.js
@@ -0,0 +1,11 @@
+function formulaire () {
+
+    var name = document.form.name.value;
+
+    var email = document.form.email.value;
+
+    var phone = document.form.phone.value;
+
+    console.log(name + " " + email + " " + phone)
+}
+
