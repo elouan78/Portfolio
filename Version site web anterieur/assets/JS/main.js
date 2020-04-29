e6e4150fbbeaf39e00e0a671d3d5c8a1fccf7298
Elouan <elouanmace.professionnel@gmail.com>
2020-04-29 17:58:56 +0200

    commit final

---
+++ Version site web anterieur/assets/JS/main.js
@@ -0,0 +1,13 @@
+function formulaire () {
+
+    var name = document.form.name.value;
+
+    var email = document.form.email.value;
+
+    var phone = document.form.phone.value;
+
+    var message = document.form.message.value;
+
+    console.log(" " + name + " " + email + " " + phone + " " + message ) 
+}
+
